3a11859c1867dee064705a7e3fb677568fd51509
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-09-10 16:17:32 +0500

    running other files with child_process

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 program
     .version('0.0.1')
@@ -19,7 +20,7 @@ program
         }
         
         const start = debounce(() => {
-            console.log('STARTING USERS PROGRAM');
+            spawn('node', [name], {stdio: 'inherit'});
         }, 100);
         
         chokidar
